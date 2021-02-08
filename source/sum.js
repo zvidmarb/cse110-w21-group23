f6b6eae3338a0985533ac9195685565bd6190b14
Colin Young <young.colin86@gmail.com>
2021-02-08 13:06:20 -0800

    demonstrating pipeline

---
+++ source/sum.js
@@ -0,0 +1,10 @@
+/**
+ * Sums two numbers
+ * @param {number} a - The first number to add.
+ * @param {number} b - The second number to add.
+ */
+function sum(a, b) {
+//note that indentation should change after linted
+return a + b;
+}
+module.exports = sum;
