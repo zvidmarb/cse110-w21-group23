d8da9d4f4052938fb173b70a4f80b0474c3d374b
crossboy <itscrossboy@gmail.com>
2021-02-11 18:06:07 -0800

    Add unit test files/structure

---
+++ source/unitTests/settings.test.js
@@ -0,0 +1,4 @@
+test("Ensures settings are changed when their respective boxes are edited", () => {
+	// Temporary test while the codebase is setup
+	expect(1).toBe(1);
+})
---
+++ source/unitTests/startstop.test.js
@@ -0,0 +1,9 @@
+test("Ensure when start/stop is pressed, the timer is reset to the max time", () => {
+	// Temporary test while the codebase is setup
+	expect(1).toBe(1);
+})
+
+test("Ensure when start/stop is pressed, text switches to the opposite", () => {
+	// Temporary test while the codebase is setup
+	expect(1).toBe(1);
+})
