94bf701d15aa1e28612af649f2bdef0a6d3b7c51
DameDollaForThree <j1cai@ucsd.edu>
2021-02-23 14:40:36 -0800

    add counter functionality and some unit tests

--- source/unitTests/counter.test.js
+++ source/unitTests/counter.test.js
@@ -1,4 +1,25 @@
-test("Ensures the counter is incremented by one after a pomo is complete", () => {
-	// Temporary test while the codebase is setup
-	expect(1).toBe(1);
-})
+beforeAll(() => {
+  document.body.innerHTML = `
+  <p id="counter">Pomo: 1&frasl;4</p>
+	<h1 id="timer">00:02</h1>
+	<button id="stop">Start</button>
+`;
+  require("../main.js");
+});
+
+test("Ensures the counter is incremented by one after a pomo is complete", (done) => {
+  const startButton = document.getElementById("stop");
+  const timer = document.getElementById("timer");
+  const enterPomo = require("../main");
+  var pomoCount = 1;
+
+  enterPomo("00:02");
+  // wait for 5 seconds for the counter to increment
+  setTimeout(() => {
+    var counter = document.getElementById("counter");
+    // the counter should be incremented by 1
+    // extract the sixth char of "Pomo: 1/4"
+    expect(counter.innerHTML.charAt(6)).toBe((pomoCount + 1).toString());
+    done();
+  }, 5000);
+});
