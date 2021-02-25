c1f3cc31a99a86a8989064b6775f5becdd2f090a
crossboy <itscrossboy@gmail.com>
2021-02-25 14:16:29 -0800

    Settings implementation + pomo duration setting

--- source/main.js
+++ source/main.js
@@ -3,10 +3,18 @@
 const startButton = document.getElementById('stop')
 const timer = document.getElementById('timer')
 const counter = document.getElementById('counter')
+const settingsButton = document.getElementById('settingsButton')
+const settingsMenu = document.getElementById('settings')
+let settings = {}
 let interval
 
 startButton.onclick = function () {
   changeButtonText()
+}
+
+settingsButton.onclick = function () {
+  settingsMenu.classList.toggle("hidden")
+  resetTimer()
 }
 
 /**
@@ -23,17 +31,19 @@ function changeButtonText () {
 }
 
 // some initial variables for convenience of future reference
-const initTime = timer.innerHTML
-const initMinute = parseInt(initTime.slice(0, 2))
-const initSecond = parseInt(initTime.slice(3))
+let initMinute = 25
 const initCounter = counter.innerHTML
 
 /**
  * Starts the timer.
  */
 function startTimer () {
+  // Close the settings menu if you try to start the timer
+  if (!settingsMenu.classList.contains("hidden")) {
+    settingsMenu.classList.toggle("hidden")
+  }
   const countDownTime =
-    new Date().getTime() + initMinute * 60 * 1000 + initSecond * 1000
+    new Date().getTime() + initMinute * 60 * 1000
   interval = setInterval(countDown, 1000, countDownTime)
 }
 
@@ -82,7 +92,97 @@ function countDown (countDownTime) {
  * Resets Timer and Pomo Counter.
  */
 function resetTimer () {
-  timer.innerHTML = initTime
+  timer.innerHTML = initMinute + ':00'
   counter.innerHTML = initCounter
   clearInterval(interval)
+}
+
+/**
+ * Adds a setting to the Pomo app
+ * @param {string} name - The internal name of the setting
+ * @param {string} title - The shown title of the setting
+ * @param {string} desc - The longer description of the setting
+ * @param {*} def - The default value of the setting
+ */
+function addSetting (name, title, desc, def, cb) {
+  // Add the setting to our list, and set it to default
+  settings[name] = def
+
+  // Create the HTML
+  let label = document.createElement("label")
+  label.setAttribute('for', name)
+  label.textContent = title
+  let input = document.createElement("input")
+  input.setAttribute('type', 'number')
+  input.setAttribute('id', name)
+  input.setAttribute('name', name)
+  settingsMenu.appendChild(label)
+  settingsMenu.appendChild(input)
+
+  // Create event listener
+  input.onchange = function (event) {
+    const newValue = input.value
+    settings[name] = newValue
+    window.localStorage.setItem(name, newValue)
+    cb(newValue)
+    resetTimer()
+  }
+}
+
+window.onload = function () {
+  // Settings
+  addSetting(
+    "pomos_before_long_break",
+    "Pomos before long break",
+    "The number of pomos that should be completed before a long break occurs.",
+    4,
+    value => {
+      console.log(value)
+    }
+  )
+  addSetting(
+    "pomo_duration", 
+    "Pomo duration",
+    "The duration each individual Pomo should be, in minutes.",
+    25,
+    value => {
+      console.log(value)
+      initMinute = value
+    }
+  )
+  addSetting(
+    "break_short_duration",
+    "Short break duration",
+    "How long each individual Short Break should be, in minutes.",
+    5,
+    value => {
+      console.log(value)
+    }
+  )
+  addSetting(
+    "break_long_duration",
+    "Long break duration",
+    "The duration each individual Long Break should be, in minutes.",
+    30,
+    value => {
+      console.log(value)
+    }
+  )
+
+
+  // Load default settings
+  for (const setting in settings) {
+    const curValue = settings[setting]
+    const settingStore = window.localStorage.getItem(setting)
+    if (settingStore == null) {
+      window.localStorage.setItem(setting, curValue)
+    } else {
+      settings[setting] = window.localStorage.getItem(setting)
+    }
+
+    document.getElementById(setting).setAttribute('value', settings[setting])
+    document.getElementById(setting).onchange()
+  }
+
+  resetTimer()
 }
