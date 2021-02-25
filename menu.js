950ed25736ddb0306a91a7551b65f54d762a59bf
la <laouinia@yahoo.com>
2021-02-25 15:51:22 -0800

    fixed menu.js

--- menu.js
+++ menu.js
@@ -1,35 +1,35 @@
 
 /* Menu js */
 
-function openNav() {
-  document.getElementById("side-task").style.width = "350px";
+function openNav () {
+  document.getElementById('side-task').style.width = '350px'
 }
 
-function closeNav() {
-  document.getElementById("side-task").style.width = "0";
+function closeNav () {
+  document.getElementById('side-task').style.width = '0'
 }
 
 /*Testing out code */
 
 // Get the info box
-var info = document.getElementById("info");
+let info = document.getElementById('info')
 
 // Open info box when the question mark is clicked
 // (We need to set this up with our question mark icon)
-var btn = document.getElementById("mybtn");
-btn.onclick = function() {
-  info.style.display = "block";
+let btn = document.getElementById('mybtn')
+btn.onclick = function () {
+  info.style.display = 'block'
 }
 
-//Close the Info Box when we click on the x
-var close = document.getElementsByClassName("close")[0];
-close.onclick = function() {
-  info.style.display = "none";
+// Close the Info Box when we click on the x
+let close = document.getElementsByClassName('close')[0]
+close.onclick = function () {
+  info.style.display = 'none'
 }
 
 // Close the Info Box if we click anywhere else on the page
-window.onclick = function(event) {
-  if (event.target == info) {
-    info.style.display = "none";
+window.onclick = function (event) {
+  if (event.target === info) {
+    info.style.display = 'none'
   }
 }
