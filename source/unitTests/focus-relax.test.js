e43571a5b585f526522aace5ec0617fa626d0cb6
TohmeNawwar <ntohme@ucsd.edu>
2021-03-01 19:37:46 -0800

    updated linter fixes

--- source/unitTests/focus-relax.test.js
+++ source/unitTests/focus-relax.test.js
@@ -1,6 +1,6 @@
 const enterShortBreak = require('../main')
-const fs = require('fs')
-const path = require('path')
+// const fs = require('fs')
+// const path = require('path')
 // const html = fs.readFileSync(path.resolve(__dirname, '../index.html'), 'utf8')
 jest
   .dontMock('fs')
