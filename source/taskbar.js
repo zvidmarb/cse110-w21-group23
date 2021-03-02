cfebf2837ebb271e6e155867223a2b5c9bef6f48
zvidmarb <50197931+zvidmarb@users.noreply.github.com>
2021-03-02 01:52:27 -0800

    Renamed taskbar related elements on index and css/js files, commented out for reference LA's test sidebar

---
+++ source/taskbar.js
@@ -0,0 +1,17 @@
+
+// Menu js
+// openNav is considered used here
+
+/* eslint-disable */
+function openNav() {
+  document.getElementById('taskbar').style.width = '350px'
+}
+/* eslint-enable */
+
+// closeNav is considered used here
+
+/* eslint-disable */
+function closeNav () {
+  document.getElementById('taskbar').style.width = '0'
+}
+/* eslint-enable */
