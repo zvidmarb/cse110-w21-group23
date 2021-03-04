7c899dbc56ef3768d3e1dcf9f41f65873e1bf9af
la <laouinia@yahoo.com>
2021-03-04 14:44:38 -0800

    testing

--- source/unitTests/infobox.test.js
+++ source/unitTests/infobox.test.js
@@ -1,22 +1,33 @@
+// const sum = require("../js/sum");
+
+
+// describe('Test if block active when it is clicked', () => {
+//     test('Click is working', () => { 
+//         btn.click()
+//         expect(info.style.display).toBe('block')
+//     })
+// })
+
+
+const info = document.getElementById("info");
+const btn = document.getElementById("mybtn");
+
+
+// test("Test the click button", () => {
+//   // Temporary test while the codebase is setup
+//   expect(1).toBe(1);
+// });
 
 
 
-
-
-
-
-
-describe('Test if block active when it is clicked', () => {
-    test('Click is working', () => { 
-        btn.click()
-        expect(info.style.display).toBe('block')
-    })
+test('Test if block active when it is clicked', () => { 
+  const btnOnclick = btn.onclick('mybtn')
+  btnOnclick.onclick()
+  expect(btnOnclick.content).toBe('info-box')
 })
 
-
-
-
-test("Test the click button", () => {
-  // Temporary test while the codebase is setup
-  expect(1).toBe(1);
-});
+// test("Test if block active when it is clicked", () => {
+//   const btnOnclick = document.getElementsById('mybtn');
+//   btnOnclick.onclick()
+//   expect(btnOnclick.inf.style.display).toBe(true);
+// });
