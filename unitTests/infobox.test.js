fe5ff1b2b2eb90e40c46966f21a4f862b8b44ff6
la <laouinia@yahoo.com>
2021-03-04 16:28:05 -0800

    Fixing Lint 02

--- unitTests/infobox.test.js
+++ unitTests/infobox.test.js
@@ -1,10 +1,8 @@
-// const sum = require("../js/sum");
 beforeAll(() => {
   document.body.innerHTML = `
     <p id='counter'>Pomo: 1&frasl;4</p>
     <h1 id='timer'>00:07</h1>
     <button id='stop'>Start</button>
-
     <i id="mybtn" class="fas fa-question"></i>
     <div id="info" class="info-box">
       <div class="info-content">
@@ -17,18 +15,18 @@ beforeAll(() => {
   require("../js/infobox.js");
 });
 
-
 describe('Test if block active when it is clicked', () => {
-    test('Click is working', () => { 
+    test('Click is working', () => {
       const btn = document.getElementById('mybtn')
       const info = document.getElementById("info")
       const close = document.getElementsByClassName("close")[0]
-      const closeInfo = require("../js/infobox.js")
+      
+      //const closeInfo = require("../js/infobox.js")
       btn.click()
       expect(info.style.display).toBe('block')
       close.click()
       expect(info.style.display).toBe('none')
-      btn.click();
-      expect(info.style.display).toBe("block");
-    })  
+      btn.click()
+      expect(info.style.display).toBe("block")
+    })
 })
