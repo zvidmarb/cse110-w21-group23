2861e9c59d771be91300486fee7324c4232b8b8f
la <laouinia@yahoo.com>
2021-03-04 16:16:24 -0800

    updated tests

--- unitTests/infobox.test.js
+++ unitTests/infobox.test.js
@@ -1,33 +1,34 @@
 // const sum = require("../js/sum");
+beforeAll(() => {
+  document.body.innerHTML = `
+    <p id='counter'>Pomo: 1&frasl;4</p>
+    <h1 id='timer'>00:07</h1>
+    <button id='stop'>Start</button>
+
+    <i id="mybtn" class="fas fa-question"></i>
+    <div id="info" class="info-box">
+      <div class="info-content">
+          <span class="close">&times;</span>
+          <h2>How to use the app!</h2>
+          <p>Something something something lorem ipsem.</p>
+      </div>
+    </div>
+`;
+  require("../js/infobox.js");
+});
 
 
-// describe('Test if block active when it is clicked', () => {
-//     test('Click is working', () => { 
-//         btn.click()
-//         expect(info.style.display).toBe('block')
-//     })
-// })
-
-
-const info = document.getElementById("info");
-const btn = document.getElementById("mybtn");
-
-
-// test("Test the click button", () => {
-//   // Temporary test while the codebase is setup
-//   expect(1).toBe(1);
-// });
-
-
-
-test('Test if block active when it is clicked', () => { 
-  const btnOnclick = btn.onclick('mybtn')
-  btnOnclick.onclick()
-  expect(btnOnclick.content).toBe('info-box')
+describe('Test if block active when it is clicked', () => {
+    test('Click is working', () => { 
+      const btn = document.getElementById('mybtn')
+      const info = document.getElementById("info")
+      const close = document.getElementsByClassName("close")[0]
+      const closeInfo = require("../js/infobox.js")
+      btn.click()
+      expect(info.style.display).toBe('block')
+      close.click()
+      expect(info.style.display).toBe('none')
+      btn.click();
+      expect(info.style.display).toBe("block");
+    })  
 })
-
-// test("Test if block active when it is clicked", () => {
-//   const btnOnclick = document.getElementsById('mybtn');
-//   btnOnclick.onclick()
-//   expect(btnOnclick.inf.style.display).toBe(true);
-// });
