f5d6d4634db90b3eb3808753b9a6856664c98ffe
Colin Young <young.colin86@gmail.com>
2021-03-04 19:43:02 -0800

    Revert "Updated layout, adding changing focus/relax icon"

--- unitTests/startstop.test.js
+++ unitTests/startstop.test.js
@@ -1,10 +1,8 @@
 beforeAll(() => {
   document.body.innerHTML = `
-  <p id='counter'>Pomo: 1&frasl;4</p>
-<h1 id='timer'>00:07</h1>
-<button id='stop'>Start</button>
-<h2 id="focus">Focus</h2>
-<h2 id="relax">Relax</h2>
+    <p id='counter'>Pomo: 1&frasl;4</p>
+    <h1 id='timer'>00:07</h1>
+    <button id='stop'>Start</button>
 `
   require('../main.js')
 })
