1d0504c41e63470b79f6b50fd91d26eab4c923cd
crossboy <itscrossboy@gmail.com>
2021-03-09 18:43:54 -0800

    Linting fixes

--- unitTests/settings.test.js
+++ unitTests/settings.test.js
@@ -1,53 +1,54 @@
 const addSetting = require('../main.js')
 
 beforeAll(() => {
-	document.body.innerHTML = `
-	<i id="cogs" class="fas fa-cog"></i>
-	<div id="settings" class="hidden">
-        <!-- <h2 id="setting-header">Settings</h2> -->
-    </div>`
+  document.body.innerHTML = `
+  <i id="cogs" class="fas fa-cog"></i>
+  <div id="settings" class="hidden">
+    <!-- <h2 id="setting-header">Settings</h2> -->
+  </div>`
 
-	require('../main.js')
+  require('../main.js')
 })
+
 describe('Unit tests to ensure settings functionality.', () => {
-	test('Make sure the settings menu opens/closes on click', () => {
-		// Get our settings box
-		const settingsMenu = document.getElementById('setting-header')
-		const settingsButton = document.getElementById('cogs')
+  test('Make sure the settings menu opens/closes on click', () => {
+    // Get our settings box
+    const settingsMenu = document.getElementById('setting-header')
+    const settingsButton = document.getElementById('cogs')
 
-		// Should start hidden
-		expect(settingsMenu.classList.contains('hidden')).toBeTruthy()
+    // Should start hidden
+    expect(settingsMenu.classList.contains('hidden')).toBeTruthy()
 
-		// Click the button
-		settingsButton.click()
+    // Click the button
+    settingsButton.click()
 
-		// Should be shown now
-		expect(settingsMenu.classList.contains('hidden')).toBeFalsy()
-	})
+    // Should be shown now
+    expect(settingsMenu.classList.contains('hidden')).toBeFalsy()
+  })
 
-	test('Ensure addSetting properly functions', () => {
-		// Setting value for testing
-		let testSetting
+  test('Ensure addSetting properly functions', () => {
+    // Setting value for testing
+    let testSetting
 
-		// See if we can add a setting
-		addSetting(
-			'test_setting',
-			'Unused',
-			'Internal setting used for testing.',
-			5,
-			value => {
-				testSetting = parseInt(value)
-			}
-		)
+    // See if we can add a setting
+    addSetting(
+      'test_setting',
+      'Unused',
+      'Internal setting used for testing.',
+      5,
+      value => {
+        testSetting = parseInt(value)
+      }
+    )
 
-		// Ensure the default is kept properly
-		expect(testSetting).toBe(5)
+    // Ensure the default is kept properly
+    expect(testSetting).toBe(5)
 
-		// Ensure we call the callback when the setting changes
-		const testSettingInput = document.getElementById("test_setting")
-		testSettingInput.val(9)
-		testSettingInput.trigger('change')
+    // Ensure we call the callback when the setting changes
+    const testSettingInput = document.getElementById('test_setting')
+    testSettingInput.val(9)
+    testSettingInput.trigger('change')
 
-		expect(testSetting).toBe(9)
-	})
+    expect(testSetting).toBe(9)
+  })
 })
