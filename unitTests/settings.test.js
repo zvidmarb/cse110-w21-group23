5bb8edfa598a424acff3549f8efcbf1b3b190028
crossboy <itscrossboy@gmail.com>
2021-03-09 18:02:31 -0800

    Add unit tests

--- unitTests/settings.test.js
+++ unitTests/settings.test.js
@@ -1,4 +1,49 @@
-test("Ensures settings are changed when their respective boxes are edited", () => {
-	// Temporary test while the codebase is setup
-	expect(1).toBe(1);
+beforeAll(() => {
+	document.body.innerHTML = `
+	<div id="settings" class="hidden">
+        <!-- <h2 id="setting-header">Settings</h2> -->
+    </div>`
+
+	require('../main.js')
+})
+
+test('Make sure the settings menu opens/closes on click', () => {
+	// Get our settings box
+	const settingsMenu = document.getElementById('setting-header')
+	const settingsButton = document.getElementById('settings-button')
+
+	// Should start hidden
+	expect(settingsMenu.classList.contains('hidden')).toBeTruthy()
+
+	// Click the button
+	settingsButton.click()
+
+	// Should be shown now
+	expect(settingsMenu.classList.contains('hidden')).toBeFalsy()
+})
+
+test('Ensure addSetting properly functions', () => {
+	// Setting value for testing
+	let testSetting
+
+	// See if we can add a setting
+	addSetting(
+		'test_setting',
+    	'Unused',
+    	'Internal setting used for testing.',
+    	5,
+    	value => {
+			testSetting = parseInt(value)
+    	}
+	)
+
+	// Ensure the default is kept properly
+	expect(testSetting).toBe(5)
+
+	// Ensure we call the callback when the setting changes
+	const testSettingInput = document.getElementById("test_setting")
+	testSettingInput.val(9)
+	testSettingInput.trigger('change')
+
+	expect(testSetting).toBe(9)
 })
